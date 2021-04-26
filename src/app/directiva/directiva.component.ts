2ac3ddcb29a5510953b124de4f0a34aaf1f1f58e
evegeniy <72518319+evegeniy@users.noreply.github.com>
2021-04-26 18:12:09 +0200

    Adding component Directiva

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+
+
+  listaCurso: string[] = ['TypeScript', 'TypeScript', 'Java SE', 'C#', 'PHP']
+
+  constructor() { }
+
+
+}
