9970c751c6d81148abfc5a3c85add67fecf62c53
evegeniy <72518319+evegeniy@users.noreply.github.com>
2021-04-28 11:55:36 +0200

    Connecting front end to backend

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import { CLIENTES } from './clientes.json';
+import { CLIENTES } from './clientes.json'; // created for testing  
 import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
 
-  constructor() { }
+  // Url to connect to the endpoint of backend
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
-     return of(CLIENTES); 
+
+    // This will return en cliente json list
+    //return of(CLIENTES); 
+    return this.http.get(this.urlEndPoint).pipe(
+      map (response => response as Cliente[])
+     );
     }
 }
